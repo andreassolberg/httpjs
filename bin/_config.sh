e549ec190e93fcb95e6f2770f2c70c7bdaf2d69b
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2017-01-03 14:49:09 +0100

    Update building script to deploy to kube.

--- bin/_config.sh
+++ bin/_config.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 export KUBERNETES_DEPLOYMENT="httpjs"
-export VERSION="1.0.2"
+export VERSION="1.0.3"
 export IMAGE="eu.gcr.io/turnkey-cocoa-720/$KUBERNETES_DEPLOYMENT:$VERSION"
--- bin/build.sh
+++ bin/build.sh
@@ -13,8 +13,8 @@ if [ "$1" == "deploy" ]; then
     echo "Pushing image to repo. Image is $IMAGE"
     gcloud docker push $IMAGE
 
-    # echo "Patching kubernetes object ${KUBERNETES_DEPLOYMENT}"
-    # kubectl patch deployment ${KUBERNETES_DEPLOYMENT} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${KUBERNETES_DEPLOYMENT}\",\"image\":\"${IMAGE}\"}]}}}}"
+    echo "Patching kubernetes object ${KUBERNETES_DEPLOYMENT}"
+    kubectl patch deployment ${KUBERNETES_DEPLOYMENT} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${KUBERNETES_DEPLOYMENT}\",\"image\":\"${IMAGE}\"}]}}}}"
     # export PODNAME=`kubectl get pods -l "appid in ($KUBERNETES_DEPLOYMENT)" -o jsonpath="{.items[0].metadata.name}"`
     # echo "New pod name is $PODNAME"
 fi
